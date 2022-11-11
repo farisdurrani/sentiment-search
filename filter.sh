ce9bb841470d5d45380583a85afd4d563e7419c3
RenChu Wang <rentrue@proton.me>
2022-11-11 16:32:56 -0500

    Up until 2014 done.

--- filter.sh
+++ filter.sh
@@ -1,6 +1,6 @@
 source $HOME/env/bin/activate
 
-for Y in 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021
+for Y in 2014 2015 2016 2017 2018 2019 2020 2021
 do
     for M in 01 02 03 04 05 06 07 08 09 10 11 12
     do
@@ -8,7 +8,7 @@ do
         ./dbxcli get /DVA_Datasets/Reddit/csv.txz/${CSV_FILE}.txz
         tar xvf ${CSV_FILE}.txz
         python reddit-filter.py -y $Y -m $M -t 50
-        zip -r out-${CSV_FILE}.zip out-${CSV_FILE}
+        zip -9 out-${CSV_FILE}.zip out-${CSV_FILE}
         ./dbxcli put out-${CSV_FILE}.zip /DVA_Datasets/Reddit/filtered.zip/${CSV_FILE}.zip
         rm ${CSV_FILE}.txz ${CSV_FILE} out-${CSV_FILE} out-${CSV_FILE}.zip
     done
