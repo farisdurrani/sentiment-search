0eb99e329190abde195e361a0c8911a1df3915ff
Faris Durrani <afarisdurrani@gmail.com>
2022-11-15 12:46:43 -0500

    Created Hello World frontend

---
+++ Project/frontend/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import React from "react";
+import { Spacer } from "./components";
+import { ToastContainer } from "react-toastify";
+import NavbarMain from "./features/NavbarMain";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <div id="app">
+      <BrowserRouter>
+        <div id="app-router">
+          <NavbarMain />
+          <Routes>
+            <Route exact path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+          <ToastContainer position="top-center" />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
