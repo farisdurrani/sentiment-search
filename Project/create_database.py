16b9e184b6ab3a40eb114d2816d302930301be4e
Justin Z <jzandstra3@gatech.edu>
2022-11-17 15:55:24 -0500

    edited file to make the sentiments to work for the twitter data and made a file to create the database

---
+++ Project/create_database.py
@@ -0,0 +1,82 @@
+import sqlite3
+import pandas as pd
+import dropbox
+import io
+from configparser import ConfigParser
+
+#df = pd.read_csv() #fill in share link from dropbox
+
+#con = sqlite3.connect("dva_database")
+#cur = con.cursor()
+
+#cur = con.execute("CREATE VIRTUAL TABLE posts USING fts4(platform TEXT, bodyText TEXT, sentiment FLOAT, date DATETIME, country TEXT)")
+#cur = con.execute("CREATE TABLE significant_events (date DATETIME, event TEXT)")
+
+config = ConfigParser()
+config.read('dropbox_token.ini')
+DROPBOX_TOKEN = config['token']['token']
+
+dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+
+#method to get the share of a file from its path
+def getShareLinkFromPath(path):
+    try: # if we have already created a sharing link, we can't create a new one, but we can get the old one
+        shared_link_metadata = dbx.sharing_create_shared_link_with_settings(path)
+    except:
+        shared_link_metadata = dbx.sharing_list_shared_links(path).links[0]
+    shared_link = shared_link_metadata.url
+    shared_link= shared_link[:-1]+'1'
+    return shared_link
+
+#method to read a file in from the path
+def readDfFromPath(path):
+    metadata, file = dbx.files_download(path=path)
+    with io.BytesIO(file.content) as stream:
+        df = pd.read_csv(stream, index_col=0, encoding='latin-1')
+    return df
+
+#method to get the names of the files in a path
+def getFileNames(path):
+    files = dbx.files_list_folder('/DVA_Datasets').entries
+
+    files_list = []
+    for file in files:
+        if isinstance(file, dropbox.files.FileMetadata):
+            files_list.append(file.name)
+    return files_list
+
+#method to fill the significant_events table with the data from two files
+def insertSigEventsFiles():
+    for file_path in sig_events_files:
+        df = readDfFromPath(file_path)
+        print(df.head())
+        for row in df:
+            date = row.date
+            event = row.event
+            query = "INSERT INTO significant_events VALUES (" + date + ", " + event + ")"
+            con.execute(query)
+
+#mathod to insert the posts of one of the paths to tagged files
+def insertPosts(folder_path):
+    path = '/DVA_Datasets' + folder_path
+    files = getFileNames(path)
+    for file_name in files:
+        print(file_name)
+        df = readDfFromPath(path+ '/' + file_name)
+        for row in df:
+            platform = row.plateform
+            bodyText = row.bodyText
+            sentiment = row.sentiment
+            date = row.date
+            country = row.country
+            query = "INSERT INTO posts VALUES (" + platform + ", " + bodyText + ", " + sentiment + ", " + date + ", " + country + ")"
+            con.execute(query)
+
+sig_events_files = ['/DVA_Datasets/significant_events.xlsx', '/DVA_Datasets/significant_events_22.xlsx']
+posts_folder_paths = ['/CNN/sentiments', 'Facebook/facebook_posts/sentiments', '/New York Times', '/Reddit/tagged', 'The Guardian/sentiments', '/twitter/sentiments']
+
+print('inserting significant events')
+insertSigEventsFiles()
+for path in posts_folder_paths:
+    print(path)
+    insertPosts(path)
