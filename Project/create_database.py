9c15d3df2997de0035ccd8b81409f505bf14588c
Justin Z <jzandstra3@gatech.edu>
2022-11-17 22:23:26 -0500

    added IF EXISTS to DROP tables

--- Project/create_database.py
+++ Project/create_database.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 con = sqlite3.connect("dva_database")
 cur = con.cursor()
-cur = con.execute("DROP TABLE posts")
-cur = con.execute("DROP TABLE significant_events")
+cur = con.execute("DROP TABLE IF EXISTS posts")
+cur = con.execute("DROP TABLE IF EXISTS significant_events")
 cur = con.execute("CREATE VIRTUAL TABLE posts USING fts4(platform TEXT, bodyText TEXT, sentiment FLOAT, date DATETIME, country TEXT)")
 cur = con.execute("CREATE TABLE significant_events (date DATETIME, event TEXT)")
 
@@ -107,9 +107,10 @@ def insertPosts(folder_path):
         df = adaptDf(path, df)
         df = df[['platform', 'bodyText', 'sentiment', 'date', 'country']]
         df.to_sql('posts', con=con, if_exists='append', index=False)
-
+        
 sig_events_files = ['/DVA_Datasets/significant_events.xlsx', '/DVA_Datasets/significant_events_22.xlsx']
 posts_folder_paths = ['/CNN/sentiments', '/Facebook/facebook_posts/sentiments', '/New York Times', '/Reddit/tagged', '/The Guardian/sentiments', '/twitter/sentiments']
+
 
 print("INSERTING POSTS")
 for path in posts_folder_paths:
