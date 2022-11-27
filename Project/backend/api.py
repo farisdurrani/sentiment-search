a66097210b2252eb91e47c89bd5773a25560d817
RenChu Wang <rentrue@proton.me>
2022-11-27 17:07:45 -0500

    done

--- Project/backend/api.py
+++ Project/backend/api.py
@@ -25,12 +25,14 @@ def get_summary():
         "orderDescending": as_bool(request_get("orderDescending")),
     }
 
+    # Helper functions to inject SQL
     start_tag = _sql_start(query["startDate"])
     end_tag = _sql_end(query["endDate"])
     platform_tag = _sql_platform(query["platform"])
 
     kw_tag = _sql_keywords(query["keywords"])
 
+    # Query on the data DB
     if any([start_tag, end_tag, platform_tag]):
         date_tags = " AND ".join([start_tag, end_tag, platform_tag])
         retrieved = database.json_from_query(
@@ -41,6 +43,7 @@ def get_summary():
     else:
         data = {}
 
+    # Query on the posts DB
     if kw_tag:
         retrieved = database.json_from_query(
             f"SELECT id, bodyText FROM posts WHERE {kw_tag}"
@@ -69,6 +72,8 @@ def get_summary():
         value[0] = datetime.fromisoformat(value[0]).date().isoformat()
 
     ppsb = namedtuple("ppsb", ["post_id", "platform", "sentiment", "bodytext"])
+
+    # Collect unique days
     unique_days: Dict[str, List[ppsb]] = {v[0]: [] for v in target_data.values()}
     for (post_id, [date, platform, sentiment, bodytext]) in target_data.items():
         unique_days[date].append(ppsb(post_id, platform, sentiment, bodytext))
@@ -106,19 +111,18 @@ def get_body_text():
     except TypeError:
         post_ids = tuple(range(1, 11))
 
+    # Query on the posts DB
     posts_sql = f"SELECT bodyText FROM posts WHERE id IN {post_ids};"
     posts = database.json_from_query(posts_sql)
 
-    others_sql = (
-        f"SELECT platform, sentiment, date, country FROM data WHERE id IN {post_ids};"
-    )
+    # Query on the data DB
+    others_sql = f"SELECT platform, sentiment, date FROM data WHERE id IN {post_ids};"
     others = database.json_from_query(others_sql)
-    # print(posts, others)
     count = len(posts)
     assert len(posts) == len(others) == len(post_ids)
 
     result = []
-    for (id, (text,), (platform, sentiment, date, _)) in zip(post_ids, posts, others):
+    for (id, (text,), (platform, sentiment, date)) in zip(post_ids, posts, others):
         result.append(
             {
                 "postId": id,
@@ -133,13 +137,112 @@ def get_body_text():
 
 
 def get_bag_of_words():
-    query = sql_bag_of_words()
-    return database.json_from_query(query)
+    query = {
+        "postId": as_list_of_int("postId"),
+        "startDate": as_date(request_get("startDate")),
+        "endDate": as_date(request_get("endDate")),
+        "platform": as_str(request_get("platform")),
+        "keywords": as_list_of_str(request_get("keywords")),
+        "limitCountOfPostsPerDate": as_int(request_get("limitCountOfPostsPerDate")),
+        "limitAmountOfWords": as_int(request_get("limitAmountOfWords")),
+        "orderBy": as_str(request_get("orderBy")),
+        "orderDescending": as_bool(request_get("orderDescending")),
+    }
+
+    order_tag = _sql_order(query["orderBy"], query["orderDescending"])
+    start_tag = _sql_start(query["startDate"])
+    end_tag = _sql_end(query["endDate"])
+    platform_tag = _sql_platform(query["platform"])
+    kws = query.get("keywords", ["dummy", "keyword"])
+
+    if any([start_tag, end_tag, platform_tag]):
+        date_tags = " AND ".join([start_tag, end_tag, platform_tag])
+        retrieved = np.array(
+            database.json_from_query(f"SELECT id FROM data WHERE {date_tags}")
+        ).tolist()
+        data = set(retrieved)
+    else:
+        data = set()
+
+    cms = namedtuple("cms", ["count", "mean_sentiment"])
+    kw_result = {}
+    for kw in kws:
+        contains_kw = np.array(
+            database.json_from_query(
+                f"SELECT id FROM posts WHERE bodyText MATCH '%{kw}%'"
+            )
+        ).squeeze()
+
+        if len(data) == 0:
+            filtered_kw = contains_kw.tolist()
+        else:
+            filtered_kw = []
+            for ckw in contains_kw:
+                if ckw in data:
+                    filtered_kw.append(ckw)
+
+        sentiment = np.array(
+            database.json_from_query(
+                f"SELECT sentiment FROM data WHERE id IN {tuple(filtered_kw)}"
+            )
+        ).squeeze()
+
+        kw_result[kw] = cms(contains_kw.size, sentiment.mean())
+
+    return {
+        "success": True,
+        "bagOfWords": [
+            {"word": w, "count": c, "meanSentiment": ms}
+            for (w, (c, ms)) in kw_result.items()
+        ],
+    }
 
 
 def get_platform_freq():
-    query = sql_platform_freq()
-    return database.json_from_query(query)
+    query = {
+        "postId": as_list_of_int("postId"),
+        "startDate": as_date(request_get("startDate")),
+        "endDate": as_date(request_get("endDate")),
+        "platform": as_str(request_get("platform")),
+        "keywords": as_list_of_str(request_get("keywords")),
+        "limitCountOfPostsPerDate": as_int(request_get("limitCountOfPostsPerDate")),
+        "limitAmountOfWords": as_int(request_get("limitAmountOfWords")),
+        "orderBy": as_str(request_get("orderBy")),
+        "orderDescending": as_bool(request_get("orderDescending")),
+    }
+
+    order_tag = _sql_order(query["orderBy"], query["orderDescending"])
+    start_tag = _sql_start(query["startDate"])
+    end_tag = _sql_end(query["endDate"])
+    kw_tag = _sql_keywords(query["keywords"])
+
+    contains_ids = set(
+        np.array(database.json_from_query(f"SELECT id FROM posts WHERE {kw_tag}"))
+        .squeeze()
+        .tolist()
+    )
+
+    f"SELECT id, platform, sentiment FROM data"
+    if any([start_tag, end_tag]):
+        date_tags = " AND ".join([start_tag, end_tag])
+        query += f" WHERE {date_tags}"
+
+    # [id, platform, sentiment]
+    retrieved = database.json_from_query(query)
+
+    ist = namedtuple("ist", ["id", "sentiment"])
+    unique_platforms: Dict[str, List[ist]] = {r[1] for r in retrieved}
+    for [id, platform, sentiment] in retrieved:
+        if id in contains_ids:
+            unique_platforms[platform].append(ist(id, sentiment))
+
+    return {
+        "success": True,
+        "frequencies": [
+            {"platform": p, "count": c, "meanSentiment": ms}
+            for (p, (c, ms)) in unique_platforms.items()
+        ],
+    }
 
 
 def _sql_order(order: str | None, desc: bool | None):
@@ -176,43 +279,3 @@ def _sql_keywords(keywords: List[str]):
         return "(" + "\n OR ".join([f"bodyText LIKE '%{kw}%'" for kw in keywords]) + ")"
     else:
         return None
-
-
-def sql_bag_of_words() -> str:
-    query = {
-        "postId": as_list_of_int("postId"),
-        "startDate": as_date(request_get("startDate")),
-        "endDate": as_date(request_get("endDate")),
-        "platform": as_str(request_get("platform")),
-        "keywords": as_list_of_str(request_get("keywords")),
-        "limitCountOfPostsPerDate": as_int(request_get("limitCountOfPostsPerDate")),
-        "limitAmountOfWords": as_int(request_get("limitAmountOfWords")),
-        "orderBy": as_str(request_get("orderBy")),
-        "orderDescending": as_bool(request_get("orderDescending")),
-    }
-
-    order_tag = _sql_order(query["orderBy"], query["orderDescending"])
-    start_tag = _sql_start(query["startDate"])
-    end_tag = _sql_end(query["endDate"])
-
-    raise NotImplementedError
-
-
-def sql_platform_freq() -> str:
-    query = {
-        "postId": as_list_of_int("postId"),
-        "startDate": as_date(request_get("startDate")),
-        "endDate": as_date(request_get("endDate")),
-        "platform": as_str(request_get("platform")),
-        "keywords": as_list_of_str(request_get("keywords")),
-        "limitCountOfPostsPerDate": as_int(request_get("limitCountOfPostsPerDate")),
-        "limitAmountOfWords": as_int(request_get("limitAmountOfWords")),
-        "orderBy": as_str(request_get("orderBy")),
-        "orderDescending": as_bool(request_get("orderDescending")),
-    }
-
-    order_tag = _sql_order(query["orderBy"], query["orderDescending"])
-    start_tag = _sql_start(query["startDate"])
-    end_tag = _sql_end(query["endDate"])
-
-    raise NotImplementedError
