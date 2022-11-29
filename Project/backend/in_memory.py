4597f14e3c5cb2d8f74e66179212f58d8df3b35c
RenChu Wang <rentrue@proton.me>
2022-11-28 22:43:25 -0500

    debugging done

--- Project/backend/in_memory.py
+++ Project/backend/in_memory.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
+from dateutil import parser
 from handler import *
 
 _PLATFORMS = {
@@ -12,10 +15,15 @@ _PLATFORMS = {
 }
 
 _DF = pd.concat([pd.read_csv(f"{p.lower()}_filtered.csv") for p in _PLATFORMS.values()])
+del _DF["country"]
 
-_DF["date"] = _DF["date"].map(lambda s: datetime.fromisoformat(s).date().isoformat())
+
+_DF["date"] = _DF["date"].map(lambda s: parser.parse(s).date().isoformat())
 _DF["bodyText"] = _DF["bodyText"].str.lower()
-_DF.index = range(len(_DF))
+_DF.dropna()
+_DF["postId"] = range(len(_DF))
+print("preprocessing done")
+print(_DF)
 
 
 def get_summary():
@@ -40,22 +48,43 @@ def get_summary():
     df = _DF.copy()
 
     if start_date:
-        df = df[df["date"] >= start_date]
+        df = df[df["date"] >= start_date.isoformat()]
 
     if end_date:
-        df = df[df["date"] <= end_date]
+        df = df[df["date"] <= end_date.isoformat()]
 
     df = pd.concat([df[df["platform"] == plat] for plat in platform])
 
     if keywords:
         for kw in keywords:
-            df = df[df["bodyText"].str.contains(kw)]
+            df = df[df["bodyText"].str.contains(kw, na=False)]
 
+    records = df.to_dict("records")
+    converted = defaultdict(list)
+
+    for record in records:
+        converted[record["date"]].append(
+            {
+                "sentiment": record["sentiment"],
+                "platform": record["platform"],
+                "postId": record["postId"],
+            }
+        )
+
+    rows = []
+    for (date, data) in converted.items():
+        rows.append(
+            {
+                "date": date,
+                "meanSentiment": sum(d["sentiment"] for d in data) / len(data),
+                "count": len(data),
+                "posts": data,
+            }
+        )
     if order_by:
-        df.sort_values(order_by, ascending=not order_desc)
+        rows.sort(key=lambda x: x[order_by], reverse=order_desc)
 
-    converted = df.tolist()
-    return {"success": True, "rows": converted}
+    return {"success": True, "rows": rows}
 
 
 def get_body_text():
@@ -69,11 +98,13 @@ def get_body_text():
     order_by = query.get("orderBy", "date")
     order_desc = query["orderDescending"]
 
-    df = _DF.iloc[post_ids]
-    if order_by:
-        df.sort_values(order_by, ascending=not order_desc)
-
+    print(_DF["postId"], post_ids)
+    df = _DF[_DF["postId"].isin(post_ids)]
     converted = df.to_dict("records")
+
+    if order_by:
+        converted.sort(key=lambda x: x[order_by], reverse=order_desc)
+
     return {"success": True, "count": len(converted), "posts": converted}
 
 
@@ -82,7 +113,7 @@ def get_bag_of_words():
         "postId": as_list_of_int("postId"),
         "startDate": as_date(request_get("startDate")),
         "endDate": as_date(request_get("endDate")),
-        "platform": as_str(request_get("platform")),
+        "platform": as_list_of_str("platform"),
         "keywords": as_list_of_str("keywords"),
         "limitCountOfPostsPerDate": as_int(request_get("limitCountOfPostsPerDate")),
         "limitAmountOfWords": as_int(request_get("limitAmountOfWords")),
@@ -101,22 +132,22 @@ def get_bag_of_words():
     per_word = query["limitAmountOfWords"]
 
     if post_ids:
-        df = _DF.iloc[post_ids]
+        df = _DF[_DF["postId"].isin(post_ids)]
     else:
         df = _DF.copy()
 
     if start_date:
-        df = df[df["date"] >= start_date]
+        df = df[df["date"] >= start_date.isoformat()]
 
     if end_date:
-        df = df[df["date"] <= end_date]
+        df = df[df["date"] <= end_date.isoformat()]
 
     if platform:
         df = pd.concat([df[df["platform"] == plat] for plat in platform])
 
     bag_of_words = []
     for kw in keywords:
-        has_kw = df[df["bodyText"].str.contains(kw)]
+        has_kw = df[df["bodyText"].str.contains(kw, na=False)]
         mean_sentiment = np.mean(has_kw["sentiment"])
         count = len(has_kw)
         bag_of_words.append(
@@ -149,23 +180,23 @@ def get_platform_freq():
     keywords = query["keywords"]
 
     if post_ids:
-        df = _DF.iloc[post_ids]
+        df = _DF[_DF["postId"].isin(post_ids)]
     else:
         df = _DF.copy()
 
     if start_date:
-        df = df[df["date"] >= start_date]
+        df = df[df["date"] >= start_date.isoformat()]
 
     if end_date:
-        df = df[df["date"] <= end_date]
+        df = df[df["date"] <= end_date.isoformat()]
 
     platform_info = []
     for platform in _PLATFORMS.keys():
         plat_df = df[df["platform"] == platform]
         for kw in keywords:
-            plat_df = plat_df[plat_df["bodyText"].str.contains(kw)]
+            plat_df = plat_df[plat_df["bodyText"].str.contains(kw, na=False)]
         count = len(plat_df)
-        mean_sentiment = np.mean(plat_df["sentiments"])
+        mean_sentiment = np.mean(plat_df["sentiment"])
         platform_info.append(
             {"platform": platform, "count": count, "meanSentiment": mean_sentiment}
         )
